b955dc7e192f63c25c5f5fe78727791eaedd446b
walangitanmiche <walangitanmiche@gmail.com>
2020-09-12 15:53:28 +0800

    1st commit

---
+++ pertemuan_4/index.js
@@ -0,0 +1,35 @@
+// Function
+
+
+function penjumlahanDuaBujurSangkar(a,b)
+{
+
+    let luasA = a*b;
+    let luasB = b*b;
+
+    let total= luasA + luasB;
+   // console.log(total)
+
+    return total; 
+ 
+}
+console.log(penjumlahanDuaBujurSangkar(8,3));
+const hasil = penjumlahanDuaBujurSangkar(8,3);
+console.log(hasil)
+
+
+// function penjumlahanDuaBujurSangkar(a,b)
+// {
+//     let total=a*a+b*b;
+//     return total; 
+// }
+// console.log(penjumlahanDuaBujurSangkar(8,3));
+// function tambah(a,b)
+// {
+//     return a+b; 
+// }
+// function kali(a,b)
+// {
+//     return a * b;
+// }
+// console.log(tambah(kali(5*2),kali (10 *2)));
