18f3433f382fbacf789a29495554f4b8b92511bf
walangitanmiche <walangitanmiche@gmail.com>
2020-09-24 13:52:36 +0800

    1st commit

---
+++ Pertemuan_11/index.js
@@ -0,0 +1,27 @@
+//Deklarasi function lama
+
+function Test(){
+    return "Function Lama";
+}
+console.log(Test());
+
+//Arrow Function
+//1.Parameter
+const Perkalian = (a,b)=>{
+    const total=a*b;
+    return total;
+}
+console.log(Perkalian(9,8));
+
+//2.Tdk dgn Parameter
+const Mhs = () => {
+    const MhsUnklab = ["Michelle","Gusti","Regina","Reinhard"];
+    return MhsUnklab;
+}
+console.log(Mhs());
+
+//Implicit Return Value
+
+const greeting = (nama) => `Hi ${nama}`;
+
+console.log(greeting('Einstein'));
