dd240fa08ad08e29a7a56ce9a0ff70d6209c1884
walangitanmiche <walangitanmiche@gmail.com>
2020-10-14 18:18:14 +0800

    1st commit

--- pertemuan_16/hello.js
+++ pertemuan_16/hello.js
@@ -1,9 +1,11 @@
-export const name = [1,2,3];
+// export const numbers = [1,2,3,4,5];
 
+// export const world = () => {
+//    console.log('Hello World');
+// }
+ 
 export const hello = (user) => {
     console.log(`Hello, ${user}`);
 }
 
-export const world = () => {
-    console.log('Hello World');
-}
+export default hello;
--- pertemuan_16/index.js
+++ pertemuan_16/index.js
@@ -1,5 +1,9 @@
-//import { hello, world } from './hello.js';
-import * as helper from './hello'
-hello('John Doe');
-world();
-console.log(helper.name)
+// import { hello, world, numbers } from './hello.js';
+
+// import * as helper from './hello.js'
+
+import func1 from './hello.js'
+
+func1('John Doe');
+// helper.world();
+// console.log(helper.numbers);
