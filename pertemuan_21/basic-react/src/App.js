da057217907afe532bcba516dc8aaf1c0a5cce20
walangitanmiche <walangitanmiche@gmail.com>
2020-11-05 23:30:02 +0800

    1st commit

---
+++ pertemuan_21/basic-react/src/App.js
@@ -0,0 +1,30 @@
+
+import './App.css';
+import Header from './components/Header';
+import Hello from './components/Hello';
+
+
+
+
+const Content = () => {
+  return (
+    <p>
+      web programming parallel A, hari selasa jam 13.00 WITA.
+      web programming parallel A, hari selasa jam 13.00 WITA.
+      web programming parallel A, hari selasa jam 13.00 WITA.
+      web programming parallel A, hari selasa jam 13.00 WITA.
+      web programming parallel A, hari selasa jam 13.00 WITA.
+    </p>
+  )
+}
+function App() {
+  return (
+    <>
+     <Header/>
+      <Hello/>
+      <Content/>
+    </>
+  );
+}
+
+export default App;
---
+++ pertemuan_21/basic-react/src/components/Hello.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Hello = () => {
+    return(
+      <>
+      <p>Hello, selamat datang di kelas web programming</p>
+      </>
+    );
+  };
+
+export default Hello;
